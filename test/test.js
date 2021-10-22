19003e600e2510fe76e0a49f44dc2a221bd79a6f
bouchnam <MOUAD.BOUCHNAF@GRENOBLE-inp.org>
2021-10-22 13:42:04 +0200

    test_passed

--- test/test.js
+++ test/test.js
@@ -37,4 +37,103 @@ contract('AuctionContract', function (accounts) {
       }
     })
   });
+
+    //Modifier Checking
+  it("Should NOT allow non owner to reveal winners", function() {
+    /**********
+    TASK 4: Call revealWinners from account 1
+    ***********/
+     return auctionInstance.revealWinners({from: accounts[1]})
+     .then(function (instance) {
+       /*
+       We are testing for a negative condition and hence this particular block will not have executed if our test case was correct. If this part is executed then we throw an error and catch the error to assert false
+       */
+       throw("Failed to check owner in reveal winners");
+     }).catch(function (e) {
+       if(e === "Failed to check owner in reveal winners") {
+         assert(false)
+       } else {
+         assert(true)
+       }
+     })
+   })
+
+  it("Should set winners", function() {
+    /**********
+    TASK 7: Call register function from account 2
+    ***********/
+    return auctionInstance.register({from: accounts[2]})
+    .then(function(result) {
+      /**********
+      TASK 8: Call register function from account 3
+      ***********/
+        return auctionInstance.register({from: accounts[3]})
+    }).then(function() {
+      /**********
+      TASK 9: Call register function from account 4
+      ***********/
+        return auctionInstance.register({from: accounts[4]})
+    }).then(function() {
+      /**********
+      TASK 10: Call bid method from accounts[2] of Auction.sol using auctionInstance and
+      pass itemId=0, count=5 as arguments
+      ***********/
+        return auctionInstance.bid(0,5,{from: accounts[2]})
+    }).then(function() {
+      /**********
+      TASK 11: Call bid method from accounts[3] of Auction.sol using auctionInstance and
+      pass itemId=1, count=5 as arguments
+      ***********/
+        return auctionInstance.bid(1,5,{from: accounts[3]})
+    }).then(function() {
+      /**********
+      TASK 12: Call bid method from accounts[4] of Auction.sol using auctionInstance and
+      pass itemId=2, count=5 as arguments
+      ***********/
+        return auctionInstance.bid(2,5,{from: accounts[4]})
+    }).then(function() {
+      /**********
+      TASK 13: Call revealWinners function from accounts[0]
+      ***********/
+        return auctionInstance.revealWinners({from: accounts[0]})
+    }).then(function() {
+      /**********
+      TASK 14: call winners function from accounts[0] to get the winner of item id 0
+      ***********/
+        return auctionInstance.winners(itemId=0, {from: accounts[0]})
+    }).then(function(result) {
+      /**********
+      TASK 15:  assert to see if the winner address is not the default address
+      HINT:     Default address is '0x0000000000000000000000000000000000000000'
+                Use notEqual method of assert
+                Parameters for notEqual : (result, default address , message);
+      ***********/
+      assert.notEqual(result, '0x0000000000000000000000000000000000000000', "the winner should not be the default address")
+      /**********
+      TASK 16: call winners function from accounts[0] to get the winner of item id 1
+      ***********/
+      return auctionInstance.winners(itemId=1, {from: accounts[0]})
+    }).then(function(result) {
+      /**********
+      TASK 17:  assert to see if the winner address is not the default address
+      HINT:     Default address is '0x0000000000000000000000000000000000000000'
+                Use notEqual method of assert
+                Parameters for notEqual : (result, default address , message);
+      ***********/
+      assert.notEqual(result, '0x0000000000000000000000000000000000000000', "the winner should not be the default address")
+      /**********
+      TASK 18: Call winners function from account 3 to get the winner of item id 2
+      ***********/
+      return auctionInstance.winners(itemId=2, {from: accounts[0]})
+    }).then(function(result) {
+      /**********
+      TASK 19:  assert to see if the winner address is not the default address
+      HINT:     Default address is '0x0000000000000000000000000000000000000000'
+                Use notEqual method of assert
+                Parameters for notEqual : (result, default address , message);
+      ***********/
+      assert.notEqual(result, '0x0000000000000000000000000000000000000000', "the winner should not be the default address")
+    })
+  });
+
 });
